2c524ef8117456d3fcd0f74e280ddef1d27496f2
Ángel H.O <ngel H.O@10.195.123.74>
2022-10-11 12:57:51 +0100

    Kata1 versión 2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,6 +1,7 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -9,8 +10,12 @@ import java.util.Date;
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Blas", new Date(49, 8, 24));
-        System.out.println(person.getName() + " tiene " + person.getAge());
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1949, 9, 24);
+        
+        Person person = new Person("Blas", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
 }
